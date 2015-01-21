984fb6dbb87b26063d9085f76c238ce91ae0c550
xine <xine@zeus.home>
2015-01-21 01:58:30 +0000

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of
    https://github.com/jlmc/schedules-builder-fx.git
    
    Conflicts:
    	Qtime-table/Qtime-table-fxdesktop/pom.xml

--- Qtime-table/Qtime-table-fxdesktop/src/main/java/org/xine/qtime/fxdesktop/backoffice/subjects/SubjectListController.java
+++ Qtime-table/Qtime-table-fxdesktop/src/main/java/org/xine/qtime/fxdesktop/backoffice/subjects/SubjectListController.java
@@ -1,24 +1,37 @@
-/* 
-* Copyright (c) 2015 Qxine <https://github.com/jlmc>
-* All Rights Reserved, unless otherwise granted permission.
-*
-* You may use and modify for private use, fork the official repository
-* for contribution purposes, contribute code, and reuse your own code.
-*/
+/*
+ * Copyright (c) 2015 Qxine <https://github.com/jlmc>
+ * All Rights Reserved, unless otherwise granted permission.
+ *
+ * You may use and modify for private use, fork the official repository
+ * for contribution purposes, contribute code, and reuse your own code.
+ */
 
 package org.xine.qtime.fxdesktop.backoffice.subjects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xine.qtime.entities.Subject;
+import org.xine.qtime.fxdesktop.backoffice.utils.ActionsTableCell;
 import org.xine.qtime.fxdesktop.controllers.StateController;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
@@ -26,6 +39,9 @@ import javafx.scene.layout.BorderPane;
  * The Class SubjectListController.
  */
 public class SubjectListController extends StateController {
+
+    /** The Constant LOGGER. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(SubjectListController.class);
 
     /**
      * The resources.
@@ -41,13 +57,21 @@ public class SubjectListController extends StateController {
     @FXML
     private URL location;
 
+    /** The search button. */
+    @FXML
+    private Button searchButton;
+
+    /** The search text. */
+    @FXML
+    private TextField searchText;
+
     /** The name column. */
     @FXML
-    private TableColumn<?, ?> nameColumn;
+    private TableColumn<Subject, String> nameColumn;
 
     /** The actions column. */
     @FXML
-    private TableColumn<?, ?> actionsColumn;
+    private TableColumn<Subject, Subject> actionsColumn;
 
     /** The root. */
     @FXML
@@ -63,15 +87,21 @@ public class SubjectListController extends StateController {
 
     /** The table. */
     @FXML
-    private TableView<?> table;
+    private TableView<Subject> table;
 
     /** The description column. */
     @FXML
-    private TableColumn<?, ?> descriptionColumn;
+    private TableColumn<Subject, String> descriptionColumn;
 
     /** The glass pane. */
     @FXML
     private BorderPane glassPane;
+
+    /** The subjects. */
+    private final ListProperty<Subject> subjects = new SimpleListProperty<>(
+            FXCollections.observableArrayList());
+
+    private final ObjectProperty<Subject> selected = new SimpleObjectProperty<Subject>(null);
 
     /*
      * (non-Javadoc)
@@ -83,22 +113,86 @@ public class SubjectListController extends StateController {
     }
 
     /**
+     * Gets the list property.
+     * @return the list property
+     */
+    public ListProperty<Subject> getListProperty() {
+        return this.subjects;
+    }
+
+    /**
      * Initialize.
      */
     @FXML
     public void initialize() {
 
-        assert this.nameColumn != null : "fx:id=\"nameColumn\" was not injected: check your FXML file 'subjectListView.fxml'.";
-        assert this.actionsColumn != null : "fx:id=\"actionsColumn\" was not injected: check your FXML file 'subjectListView.fxml'.";
-        assert this.root != null : "fx:id=\"root\" was not injected: check your FXML file 'subjectListView.fxml'.";
-        assert this.createButton != null : "fx:id=\"createButton\" was not injected: check your FXML file 'subjectListView.fxml'.";
-        assert this.title != null : "fx:id=\"title\" was not injected: check your FXML file 'subjectListView.fxml'.";
-        assert this.table != null : "fx:id=\"table\" was not injected: check your FXML file 'subjectListView.fxml'.";
-        assert this.descriptionColumn != null : "fx:id=\"descriptionColumn\" was not injected: check your FXML file 'subjectListView.fxml'.";
-        assert this.glassPane != null : "fx:id=\"glassPane\" was not injected: check your FXML file 'subjectListView.fxml'.";
+        // definition of cell factory
+        this.nameColumn.setCellValueFactory(cdf -> new SimpleStringProperty(cdf.getValue()
+                .getName()));
+        this.descriptionColumn.setCellValueFactory(cdf -> new SimpleStringProperty(cdf.getValue()
+                .getDescription()));
+        this.actionsColumn.setCellFactory(tc -> {
+            final ActionsTableCell<Subject, Subject> cell = new ActionsTableCell<>(false);
 
+            cell.setOnDeleteAction(e -> {
+
+                final Subject s = cell.getData();
+                this.table.getSelectionModel().select(s);
+                delete(s);
+            });
+
+            cell.setOnEditAction(e -> {
+                final Subject s = cell.getData();
+                this.table.getSelectionModel().select(s);
+                edit(s);
+            });
+
+            return cell;
+        });
+
+        this.table.setItems(getListProperty());
+
+        // /
         this.createButton.setOnAction(e -> getMachineStatesController().setActiveController(
                 getMachineStatesController().getCreateController()));
+
+        this.searchButton.setOnAction(e -> search());
+    }
+
+    /**
+     * Edits the.
+     * @param data
+     *            the data
+     */
+    private void edit(final Subject data) {
+        LOGGER.info("edit:  " + data.getName());
+    }
+
+    /**
+     * Delete.
+     * @param s
+     *            the s
+     */
+    private void delete(final Subject s) {
+        LOGGER.info("delete: " + s.getName());
+
+    }
+
+    /**
+     * Search.
+     */
+    private void search() {
+
+        // TODO Auto-generated method stub
+        LOGGER.info("Logger operation not implemented");
+
+        final List<Subject> ss = new ArrayList<>();
+        for (int i = 0; i < 150; i++) {
+            ss.add(new Subject(Long.valueOf(i), "name" + i, "N" + i, "the name " + i));
+        }
+
+        getListProperty().clear();
+        getListProperty().addAll(ss);
     }
 
 }
