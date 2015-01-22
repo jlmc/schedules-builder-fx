3ae525c299dec3ca09666815cf70a390d79044be
jlmc <costajlmpp@gmail.com>
2015-01-22 14:12:36 +0000

    add resources

---
+++ Qtime-table/Qtime-table-client/Qtime-table-client-connector-rest/src/main/java/org/xine/qtime/client/connector/rest/SubjectConnectorServiceAsync.java
@@ -0,0 +1,5 @@
+package org.xine.qtime.client.connector.rest;
+
+public class SubjectConnectorServiceAsync {
+
+}
---
+++ Qtime-table/Qtime-table-fxdesktop/src/main/java/org/xine/qtime/fxdesktop/utils/ResourceUtils.java
@@ -0,0 +1,48 @@
+/* 
+ * Copyright (c) 2015 Qxine <https://github.com/jlmc>
+ * All Rights Reserved, unless otherwise granted permission.
+ *
+ * You may use and modify for private use, fork the official repository
+ * for contribution purposes, contribute code, and reuse your own code.
+ */
+
+package org.xine.qtime.fxdesktop.utils;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * The Class ResourceUtils.
+ */
+public class ResourceUtils {
+
+	/** The Constant RESOURCE_PATH. */
+	public static final String RESOURCE_PATH = "/org/xine/qtime/fxdesktop/resources/";
+
+	/**
+	 * Gets the resource as stream.
+	 *
+	 * @param path
+	 *            the path
+	 * @return the resource as stream
+	 */
+	public static InputStream getResourceAsStream(String path) {
+		InputStream stream = ResourceUtils.class
+				.getResourceAsStream(RESOURCE_PATH + path);
+		if (stream == null) {
+			// Eclipse path
+			File resource = new File(".\\src\\main\\resources\\" + path);
+			if (resource.exists()) {
+				try {
+					stream = new BufferedInputStream(new FileInputStream(
+							resource));
+				} catch (IOException ignore) {
+				}
+			}
+		}
+		return stream;
+	}
+}
