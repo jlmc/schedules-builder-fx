494d71b4fc0744d430bfa7a5dde917db1f8ce9a3
JLC <JLC@TITAN.home>
2015-11-09 22:19:14 +0000

    remove resources

--- Qtime-table/Qtime-table-fxdesktop/init.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-
-rem java -jar Qtime-table-fxdesktop-0.0.1.jar -Djava.util.logging.config.file=log.conf
-
-java -cp target\classes\org\xine\qtime\fxdesktop\Launcher -Djava.util.logging.config.file=log.conf
